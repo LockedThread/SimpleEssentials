166ca4763e7c60b9c1bda4607063ebe650b434a4
ProteinDev <proteinshakedev@gmail.com>
2019-01-27 15:54:14 -0500

    Made Kit.java final

--- src/main/java/rip/simpleness/simpleessentials/objs/Kit.java
+++ src/main/java/rip/simpleness/simpleessentials/objs/Kit.java
@@ -6,7 +6,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class Kit {
+public final class Kit {
 
     private final String name;
     private final ItemStack[] inventoryContents;
